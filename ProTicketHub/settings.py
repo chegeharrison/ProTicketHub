e2bf276a208cdabe256f152e18e57158e4dcd20a
harrry <chegeharrison10@gmail.com>
2023-12-29 14:44:58 +0300

    updated allowed host

--- ProTicketHub/settings.py
+++ ProTicketHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+!2&_o-s19ck%bvcmgup+ss6@3)9vpk7hba01pdv%i!i8m^td!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
